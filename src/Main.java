9687027dac9922c5bfffe8bffa14ae057de8ec5c
Anders Brams <anders@brams.dk>
2017-10-09 11:27:44 +0200

    Initial structural setup, imported libraries
    Created a small HelloWorld application, imported the NXT libraries and
    linked them to the Java project.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import lejos.nxt.Button;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Iaro ist und faglord");
+        Button.waitForAnyPress();
+    }
+}
