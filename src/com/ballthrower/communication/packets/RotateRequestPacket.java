ec4b390e4758e63a731f99a407289a54ad46936e
Anders Jakobsen <andersjkbsn@gmail.com>
2017-10-23 13:54:41 +0200

    Further work on communication

---
+++ src/com/ballthrower/communication/packets/RotateRequestPacket.java
@@ -0,0 +1,37 @@
+package com.ballthrower.communication.packets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class RotateRequestPacket extends Packet
+{
+    private float _degreesToRotate = 0;
+
+    public float getDegreesToRotate()
+    {
+        return this._degreesToRotate;
+    }
+
+    public void setDegreesToRotate(float degrees)
+    {
+        this._degreesToRotate = degrees;
+    }
+
+    @Override
+    public void constructFromStream(DataInputStream stream) throws IOException
+    {
+        this.setDegreesToRotate(stream.readFloat());
+    }
+
+    @Override
+    public void writeToStream(DataOutputStream stream) throws IOException
+    {
+    }
+
+    @Override
+    public byte getId()
+    {
+        return 0x2;
+    }
+}
