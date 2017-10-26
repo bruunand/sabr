29353c32b335910344200072ac9ec5d3a66cedc1
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2017-10-26 15:03:42 +0200

    Added distance calculation

--- src/com/ballthrower/targeting/DirectionCalculator.java
+++ src/com/ballthrower/targeting/DirectionCalculator.java
@@ -1,6 +1,8 @@
 package com.ballthrower.targeting;
 
 
+import static java.lang.Math.sqrt;
+
 public class DirectionCalculator implements IDirectionCalculateable
 {
     private static final float _degreesPerPixel = 0.1017F;
@@ -12,14 +14,14 @@ public class DirectionCalculator implements IDirectionCalculateable
         float frameMid = target.getFrameMid();
         for(int i=0;i<iterations;++i)
         {
-            int xTopLeft = target.GetXPostion(i);
+            int xTopLeft = target.GetXTopPos(i);
             float height = target.GetHeight(i);
             float xCentre = xTopLeft + height/2;
-            vec2lineX = (frameMid-xCentre);
-            sumDistances += (float)sqrt(vec2lineX*vec2lineX)
+            float vec2lineX = (frameMid-xCentre);
+            sumDistances += (float)sqrt(vec2lineX*vec2lineX);
         }
-        int meanDistance = sumDistances/iterations;
-        int angle2Rotate = meanDistance * _degreesPerPixel;
+        float meanDistance = sumDistances/iterations;
+        float angle2Rotate =  meanDistance * _degreesPerPixel;
         return angle2Rotate;
     }
 }
--- src/com/ballthrower/targeting/DistanceCalculator.java
+++ src/com/ballthrower/targeting/DistanceCalculator.java
@@ -1,10 +1,59 @@
 package com.ballthrower.targeting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
 public class DistanceCalculator implements IDistanceCalculateable
 {
+    private static final float _targetHeight = 9;
+
+    private static final float _focalLengthHeight = 142.5f *29.5f / _targetHeight;
+
     @Override
     public float calculateDistance(ITargetBoxInfo target)
     {
-        return -1;
+        float[] heightList = target.GetHeightList();
+
+        float median = getMedian(heightList);
+
+        float maxDeviance = median * 0.05f;
+        float deviance = 0;
+        ArrayList<Float> tmp = new ArrayList<>();
+
+        for(int i = 0; i < heightList.length; i++)
+        {
+            deviance = abs(median - heightList[i]);
+
+            if(deviance <= maxDeviance)
+                tmp.add(heightList[i]);
+        }
+
+        float[] refinedHeightList = new float[tmp.size()];
+
+        for(int i = 0; i < tmp.size(); i++) {
+            refinedHeightList[i] = tmp.get(i);
+        }
+
+        if(refinedHeightList.length == 0)
+            // Query the camera for a new set of data.
+            return -1;
+
+        median = getMedian(refinedHeightList);
+
+        return _focalLengthHeight * _targetHeight / median;
     }
+
+    private float getMedian(float[] arr) {
+        float median;
+        Arrays.sort(arr);
+        if (arr.length % 2 == 0)
+            median = (arr[arr.length/2] + arr[arr.length/2 - 1])/2;
+        else
+            median = arr[arr.length/2];
+
+        return median;
+    }
+
 }
