99cef1b93d0f6b4f1032b7abb446276057c51659
Anders Jakobsen <andersjkbsn@gmail.com>
2017-11-08 14:33:34 +0100

    Added abortable to connection factory

--- src/com/ballthrower/communication/ConnectionFactory.java
+++ src/com/ballthrower/communication/ConnectionFactory.java
@@ -1,9 +1,22 @@
 package com.ballthrower.communication;
 
-public class BluetoothConnectionFactory implements ConnectionFactory
+import com.ballthrower.abortion.IAbortable;
+
+public class ConnectionFactory
 {
-    public Connection createInstance()
+    public Connection createInstance(ConnectionType type, IAbortable abortable)
     {
-        return new BluetoothConnection();
+        switch (type)
+        {
+            case Bluetooth:
+                return new BluetoothConnection(abortable);
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
+    public enum ConnectionType
+    {
+        Bluetooth
     }
 }
