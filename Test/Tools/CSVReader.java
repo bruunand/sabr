7d788dac3fd28d85dd3d1fe5326d71ca8b027afb
Anders Brams <anders@brams.dk>
2017-11-15 12:22:27 +0100

    getRawData added

--- Test/Tools/CSVReader.java
+++ Test/Tools/CSVReader.java
@@ -1,9 +1,40 @@
 package Tools;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Used for reading TargetBoxInfo data for unit tests
  */
 public final class CSVReader
 {
-     
+    /* path should be "\data\test_data_distance.csv" */
+
+    private String getRawData(String path)
+    {
+        try
+        {
+            File file = new File("a.txt");
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+            return new String(data, "UTF-8");
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("The file " + path + " was not found. ");
+            e.printStackTrace();
+            return null;
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
