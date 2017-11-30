fcea0045912151775f7d1eafdf32f08580a78728
mysjkin <iarogolo@gmail.com>
2017-11-30 10:27:32 +0100

    Comments, dead code removed, and bug fixed.

--- src/host/TargetInfo.py
+++ src/host/TargetInfo.py
@@ -7,27 +7,41 @@ from utils import label_map_util
 from Interfaces import ITargetInfo
 from PIL import Image
 
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
 
 
 class TargetInfo(ITargetInfo):
-    #Used in height calculation.
-    KNOWN_HEIGHT = 11.5
-    focallength = 127.5 * 60 / 11.5
-    focallength_no_enhance = 66 * 234 / 11.5
+    """ 
+    This class is used for capturing frames of the environment
+    and provide target object information to an embedded system
+    
+    Attributes:
+        MODEL_NAME (string): Path to folder where the neural network object
+            detection model resides. 
+        PATH_TO_CKPT (string): Path to frozen detection graph. 
+            This is the actual model that is used for the object detection.
+        PATH_TO_LABELS (string): Path to the list labels used to classify
+            detected objects.
+        NUM_CLASSES (integer): number of categories for classification.
+        detection_graph (object): a computation graph. 
+        label_map (list): list of labels.
+        categories (list): list of dictionaries representing all possible categories.
+        category_index (dictionary): a dictionary of the same entries as categories but
+            the key value is a category id.
 
-    #The model used to find the redcup
+    Todo:
+        *
+    """
     MODEL_NAME = 'redcuprcnn'
 
-    # Path to frozen detection graph. This is the actual model that is used for the object detection.
     PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
-    # List of the strings that is used to add correct label for each box.
     PATH_TO_LABELS = os.path.join(MODEL_NAME, 'label_map.pbtxt')
 
-    #Redcup class number in tensorflow
     NUM_CLASSES = 1
 
-    #Tensorflow graph
     detection_graph = tf.Graph()
 
     label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
@@ -37,6 +51,8 @@ class TargetInfo(ITargetInfo):
     # Initialize TargetInfo with default capture device set to 0 and sample size set to 10
     def __init__(self, capture_device=1, sample_size=10, target_width=8.25, target_height=10.5, debug=True):
         self._camera = cv2.VideoCapture(capture_device)
+        #self._camera.set(3,1600)
+        #self._camera.set(4,1200)
         self._sample_size = sample_size
         self._debug = debug
         self._aspect_ratio = target_width / target_height
@@ -51,6 +67,20 @@ class TargetInfo(ITargetInfo):
 
 
     def get_targets(self):
+        """
+        get_targets() gathers the necessary data need by the 
+        embedded system to calculate the direction or distance.
+
+        args:
+            
+        returns:
+            A tuple contaning a list of bounding boxes and 
+            an integer representing the frame width 
+
+        todo:
+            *
+        """
+
         # Retrieve a list of sample data to be processed
         sample_data = self.get_sample_data()
 
@@ -64,9 +94,26 @@ class TargetInfo(ITargetInfo):
         return (bounding_boxes, frame_width)
 
     def image_processing(self, sample_data):
+        """
+        image_processing() processes a collection of frames.
+        It uses the neural network object detection model
+        to detect red cups and uses these results to dynamically calculate the
+        colour ranges for colour and contouring which sets the final bounding box
+        around the red cups. 
+
+        args:
+            sample data: an integer representing the number of frames to process.
+        return:
+            bounding_boxes: a list of 4-tuples each 
+                having the following form [top_x_pos,top_y_pos,width,height].
+
+        todo:
+            * (maybe) split this function into smaller functions.
+
+        """
         bounding_boxes = []
 
-        # For help on colorspaces: https://docs.opencv.org/3.2.0/df/d9d/tutorial_py_colorspaces.html
+        # Colour ranges for colour and contouring
         lower_hsv_colour = np.array([0, 0, 0])
         upper_hsv_colour = np.array([0, 0, 0])
 
@@ -91,55 +138,42 @@ class TargetInfo(ITargetInfo):
                         [boxes, scores, classes, num_detections],
                         feed_dict={image_tensor: image_np_expanded})
 
-                    #Set frame size to 800x600 px. using antialiasing 
-                    frame = Image.fromarray(frame).resize((800, 600), Image.ANTIALIAS)
-                    frame = np.array(frame)
-
-                    #Removes big colour changes with median blur
-                    frame = cv2.medianBlur(frame, 3)
-
                     percent_difference = 0.40
+
                     height, width, _ = np.shape(frame)
+                    # Squeeze all detected boxes into a list of tuples
                     y_min, x_min, y_max, x_max = np.squeeze(boxes)[0]
+                    # Normalize the positions
                     y_min = int(y_min * height)
                     x_min = int(x_min * width)
                     y_max = int(y_max * height)
                     x_max = int(x_max * width)
-                    centre_x,centre_y = int((x_max+x_min)/2),int((y_max+y_min)/2)
 
+                    centre_x,centre_y = int((x_max+x_min)/2),int((y_max+y_min)/2)
+                    # Get the colour value at the center of bounding box
                     colour = frame[centre_y,centre_x]
                     colour = np.uint8([[[int(colour[0]),int(colour[1]),int(colour[2])]]])
-
                     # Convert the current frame with a BGR color profile to a frame with a HSV color profile
-                    hsv_c = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-                    hsv_1dcolor = np.array(hsv_c[0][0])
+                    hsv_colour = cv2.cvtColor(colour, cv2.COLOR_BGR2HSV)
+                    hsv_1dcolor = np.array(hsv_colour[0][0])
 
+                    # Set the colour bounds based on the colour at the center of the
+                    ## bounding box.
                     for i in range(3):
                         lower_hsv_colour[i] = int(hsv_1dcolor[i]*(1-percent_difference))
                         upper_hsv_colour[i] = int(hsv_1dcolor[i]*(1+percent_difference))
-                      
-                    hsv_blue = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-                    mask_blue = cv2.inRange(hsv_blue, lower_hsv_colour, upper_hsv_colour)
+                    hsv_red = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-                    # Create contours for all objects in the defined colorspace (See: http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_hierarchy/py_contours_hierarchy.html)
-                    _, contours, _ = cv2.findContours(mask_blue.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                    mask_red = cv2.inRange(hsv_red, lower_hsv_colour, upper_hsv_colour)
 
-                    '''# Create a mask of the image (everything within the threshold appears white and everything else is black)
-                    mask = cv2.inRange(hsv_c, lower_hsv_colour, upper_hsv_colour)
-
-                    # Get rid of background noise using erosion
-                    element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-                    mask = cv2.erode(mask, element, iterations=2)
-                    mask = cv2.dilate(mask, element, iterations=2)
-                    mask = cv2.erode(mask, element)
-
-                    # Create contours for all objects in the defined colorspace (See: http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_hierarchy/py_contours_hierarchy.html)
-                    _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)'''
+                    # Create contours for all objects in the defined colorspace
+                    _, contours, _ = cv2.findContours(mask_red.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
                     if len(contours) == 0:
                         continue
-
+                    
+                    # Get the largest contour
                     contour = max(contours, key=cv2.contourArea)
                     area = cv2.boundingRect(contour)
                     size = cv2.contourArea(contour)
@@ -147,38 +181,20 @@ class TargetInfo(ITargetInfo):
                     # Continue if no bounding box is found
                     if size == 0:
                         continue
-
-                    # Search through the countours for the largest object
-                    #best_contour = max(contours, key=cv2.contourArea)
-
-                    # Create a bounding box around the contour
-                    #bounding_boxes.append(cv2.boundingRect(best_contour))
-
-                    # DRAWING BB AND LENGTH
-                    height = area[3]
-                    cv2.rectangle(frame,(area[0],area[1]),(area[0]+area[2],area[1]+area[3]),(0,255,0),2)
-                    length_to_cup = (self.focallength_no_enhance*self.KNOWN_HEIGHT) / height
-                    print(length_to_cup)
-                    # END DRAWING AND LENGTH
-
-                    #cv2.imshow('object detection', frame)
-                    #cv2.imwrite(self.image_name,frame)
+                    bounding_boxes.append(area)
                     print("iaro ist und faglord")
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
 
         # Refine bounding box set
-        bounding_boxes = self.remove_outliers(bounding_boxes)
+        ## OBS !!!! Currently removes too much !!!! OBS
+        #bounding_boxes = self.remove_outliers(bounding_boxes)
 
 
         # If debugging is enabled draw all bounding boxes on the first frame and show the result
         if self._debug and len(sample_data) > 0:
             
             base_image = sample_data[0]
-
             for box in bounding_boxes:
                 cv2.rectangle(base_image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0, 255, 0), 3)
-                print(box)
 
             cv2.imshow('debug', base_image)
             cv2.waitKey(1)
@@ -187,6 +203,25 @@ class TargetInfo(ITargetInfo):
         return bounding_boxes
 
     def remove_outliers(self, bounding_boxes, max_deviance = 20, max_return_size = 100):
+        """
+        remove_outliers(): a simple outlier_detection based on a red cups aspect ratio.
+
+        args:
+            bounding_boxes: a list of 4-tuples representing the calculated bounding
+                boxes around the red cups.
+            max_deviance: integer representing the maximum amount the aspect ration
+                of each bounding can deviate.
+            max_return_size: integer defining the maximum amount of bounding boxes
+                that can be returned by this function.
+        return:
+            refined_set: a list of bounding boxes where obvoius outliers have been
+                removed.
+        todo:
+            * Mikkel Jarlund comment this function
+            * Removes all bounding boxes with the new object detection method
+            ** PLZ FIX
+
+        """
         refined_dict = {}
         refined_set = []
 
@@ -210,6 +245,14 @@ class TargetInfo(ITargetInfo):
         return refined_set
 
     def get_sample_data(self):
+        """
+        get_sample_data(): uses the capture device to take a number
+            of fames, this number is provided through the class contructor.
+
+        retuns:
+            sample_data: a list of arrays each representing a captured frame.
+
+        """
         sample_data = []
 
         # Add x frames to the sample data collection
