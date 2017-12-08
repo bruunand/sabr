6d5ede23f642316f83fe90c3fecd1469baa4bf00
Anders Jakobsen <andersjkbsn@gmail.com>
2017-12-08 11:02:00 +0100

    Completed closest direction policy

--- src/com/ballthrower/targeting/policies/ClosestDirectionPolicy.java
+++ src/com/ballthrower/targeting/policies/ClosestDirectionPolicy.java
@@ -1,6 +1,10 @@
+package com.ballthrower.targeting.policies;
+
 import com.ballthrower.targeting.TargetBox;
 import com.ballthrower.targeting.TargetContainer;
-import com.ballthrower.targeting.policies.Policy;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class ClosestDirectionPolicy extends Policy
 {
@@ -10,5 +14,23 @@ public class ClosestDirectionPolicy extends Policy
         if (targetContainer.getTargetCount() == 0)
             return null;
 
+        /* We are not allowed to directly access the array of targets, so we clone the array. */
+        TargetBox[] clonedArray = targetContainer.cloneTargets();
+
+        /* This policy sorts targets by their middle x-position.
+           We want the lowest of such values, as that is the target requiring the least rotation.
+         */
+        Arrays.sort(clonedArray, new DistanceComparator());
+
+        return clonedArray[0];
+    }
+
+    public class DistanceComparator implements Comparator<TargetBox>
+    {
+        @Override
+        public int compare(TargetBox first, TargetBox second)
+        {
+            return ((Float) first.getMiddleX()).compareTo(second.getMiddleX());
+        }
     }
 }
