4fa0feaca070ee71c9c40a48269c927bb82778f6
Anders Jakobsen <andersjkbsn@gmail.com>
2017-12-08 11:11:54 +0100

    Updated closest direction policy. References to TargetContainer now use the interface

--- src/com/ballthrower/targeting/policies/ClosestDirectionPolicy.java
+++ src/com/ballthrower/targeting/policies/ClosestDirectionPolicy.java
@@ -1,7 +1,7 @@
 package com.ballthrower.targeting.policies;
 
+import com.ballthrower.targeting.ITargetContainer;
 import com.ballthrower.targeting.TargetBox;
-import com.ballthrower.targeting.TargetContainer;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -9,7 +9,7 @@ import java.util.Comparator;
 public class ClosestDirectionPolicy extends Policy
 {
     @Override
-    public TargetBox getTargetBox(TargetContainer targetContainer)
+    public TargetBox getTargetBox(ITargetContainer targetContainer)
     {
         if (targetContainer.getTargetCount() == 0)
             return null;
@@ -20,17 +20,27 @@ public class ClosestDirectionPolicy extends Policy
         /* This policy sorts targets by their middle x-position.
            We want the lowest of such values, as that is the target requiring the least rotation.
          */
-        Arrays.sort(clonedArray, new DistanceComparator());
+        Arrays.sort(clonedArray, new DistanceComparator(targetContainer));
 
         return clonedArray[0];
     }
 
-    public class DistanceComparator implements Comparator<TargetBox>
+    class DistanceComparator implements Comparator<TargetBox>
     {
+        private final float _frameMiddle;
+
+        DistanceComparator(ITargetContainer container)
+        {
+            this._frameMiddle = container.getFrameWidth() / 2;
+        }
+
         @Override
         public int compare(TargetBox first, TargetBox second)
         {
-            return ((Float) first.getMiddleX()).compareTo(second.getMiddleX());
+            Float firstValue  = Math.abs(first.getMiddleX() - this._frameMiddle);
+            Float secondValue = Math.abs(second.getMiddleX() - this._frameMiddle);
+
+            return firstValue.compareTo(secondValue);
         }
     }
 }
--- src/com/ballthrower/targeting/policies/Policy.java
+++ src/com/ballthrower/targeting/policies/Policy.java
@@ -1,9 +1,9 @@
 package com.ballthrower.targeting.policies;
 
+import com.ballthrower.targeting.ITargetContainer;
 import com.ballthrower.targeting.TargetBox;
-import com.ballthrower.targeting.TargetContainer;
 
 public abstract class Policy
 {
-    public abstract TargetBox getTargetBox(TargetContainer targetContainer);
+    public abstract TargetBox getTargetBox(ITargetContainer targetContainer);
 }
