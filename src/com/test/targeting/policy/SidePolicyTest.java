55709775c7dcf86cb70c15827b16042fa98624ff
Anders Brams <anders@brams.dk>
2017-12-12 13:46:14 +0100

    SidePolicy - single target test

--- src/com/test/targeting/policy/SidePolicyTest.java
+++ src/com/test/targeting/policy/SidePolicyTest.java
@@ -1,6 +1,7 @@
 package com.test.targeting.policy;
 
 import com.ballthrower.exceptions.AssertException;
+import com.ballthrower.targeting.TargetBox;
 import com.ballthrower.targeting.TargetContainer;
 import com.ballthrower.targeting.policies.SideFirstPolicy;
 import com.test.NXTAssert;
@@ -9,12 +10,14 @@ import com.test.Test;
 
 public class SidePolicyTest extends Test
 {
-    SideFirstPolicy policy;
+    SideFirstPolicy policyLeft;
+    SideFirstPolicy policyRight;
     TargetContainer testContainer;
 
     private void setUp()
     {
-        policy = new SideFirstPolicy(SideFirstPolicy.Side.Left);
+        policyLeft = new SideFirstPolicy(SideFirstPolicy.Side.Left);
+        policyRight = new SideFirstPolicy(SideFirstPolicy.Side.Right);
         testContainer = NXTTest.getTestTargetBox();
     }
     private void zeroSampleTest() throws AssertException
@@ -22,13 +25,28 @@ public class SidePolicyTest extends Test
         TargetContainer zeroSample = new TargetContainer((byte)0);
 
         NXTAssert test = new NXTAssert();
-        test.assertThat(policy.selectTargetBox(zeroSample), "SidePolicy:zeroSample")
+        test.assertThat(policyLeft.selectTargetBox(zeroSample), "SidePolicy:zeroSample")
                 .isNull();
+        test.assertThat(policyRight.selectTargetBox(zeroSample), "SidePolicy:zeroSample")
+                .isNull();
+    }
+
+    private void singleTargetTest() throws AssertException
+    {
+        TargetContainer singleTarget = new TargetContainer((byte)1);
+        singleTarget.setTarget((byte)0, new TargetBox((short)50, (short)50, (short)50));
+
+        NXTAssert test = new NXTAssert();
+        test.assertThat(policyLeft.selectTargetBox(singleTarget), "SidePolicy:singleSample")
+                .isEqualTo(singleTarget.getTarget((byte)0));
+        test.assertThat(policyRight.selectTargetBox(singleTarget), "SidePolicy:singleSample")
+                .isEqualTo(singleTarget.getTarget((byte)0));
     }
 
     @Override
     public void runAllTests() throws AssertException {
         setUp();
         zeroSampleTest();
+        singleTargetTest();
     }
 }
