aec60176a46c21a71615567775f51c263994e6f8
Anders Brams <anders@brams.dk>
2017-12-12 10:25:38 +0100

    Trying regulated motor for rotating

--- src/com/ballthrower/movement/aiming/Rotator.java
+++ src/com/ballthrower/movement/aiming/Rotator.java
@@ -1,15 +1,16 @@
 package com.ballthrower.movement.aiming;
 
 import com.ballthrower.movement.MotorController;
-import lejos.nxt.MotorPort;
-import lejos.nxt.NXTMotor;
-import lejos.nxt.Sound;
-import lejos.nxt.LCD;
+import lejos.nxt.*;
+import lejos.robotics.RegulatedMotor;
 
 public class Rotator extends MotorController implements IRotator
 {
     /* getGearRatio = robot.numberOfGearTeeth / motor.numberOfGearTeeth. */
     private static final int MOTOR_POWER = 20;
+
+    NXTRegulatedMotor _regMotor;
+
 
     /* Updates every time we turn so we can reset position. */
     private int currentHeading = 0;
@@ -19,12 +20,15 @@ public class Rotator extends MotorController implements IRotator
     public Rotator(MotorPort motor)
     {
         super(new NXTMotor(motor), 2.33f);
+        _regMotor = new NXTRegulatedMotor(MotorPort.C);
+        _regMotor.setSpeed(MOTOR_POWER);
     }
 
     /**
      * Rotate a specific number of degrees
      * @param degrees number of degrees to rotate
      */
+    /*
     public void turnDegrees(float degrees)
     {
         int actualDegrees = (int)(degrees * getGearRatio());
@@ -39,6 +43,20 @@ public class Rotator extends MotorController implements IRotator
         super.waitWhileTurning(actualDegrees);
         Sound.buzz();
         super.stopMotors();
+
+        currentHeading += actualDegrees;
+    } */
+
+    public void turnDegrees(float degrees)
+    {
+        int actualDegrees = (int)(degrees * getGearRatio());
+
+        LCD.drawString("Turn: " + degrees, 0, 1);
+        LCD.drawString("Actual:" + actualDegrees, 0, 2);
+
+        Sound.beep();
+        _regMotor.rotate(actualDegrees);
+        Sound.buzz();
 
         currentHeading += actualDegrees;
     }
