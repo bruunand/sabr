e61334bf45fd755cde1461db29a63c079875b718
Anders Jakobsen <andersjkbsn@gmail.com>
2017-12-18 13:59:16 +0100

    Upload file working with code revisiions

--- upload_bt.sh
+++ upload_bt.sh
@@ -1,6 +1,6 @@
 cd src
 find . -type f -name '*.class' -delete
-../leJOS-Linux/bin/nxjc com/ballthrower/Main.java
+../leJOS-Linux/bin/nxjc com/sabr/Main.java
 while true
 do
     ../leJOS-Linux/bin/nxj -r -b com.sabr.Main
